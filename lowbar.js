9fe69080dc79beb577345eb0c90ae7c79956d14c
Matthew Clark <matthewclark86@gmail.com>
2018-02-24 11:19:30 +0000

    completed identity function and started values

--- lowbar.js
+++ lowbar.js
@@ -1,7 +1,12 @@
 const _ = {};
 
-_.identity = function () {
+_.identity = function (val) {
   // your code here 
+  return val; 
+};
+
+_.values = function (obj) {
+  if(typeof obj !== 'object') return [];
 };
 
 module.exports = _;
--- spec/lowbar.js
+++ spec/lowbar.js
@@ -14,5 +14,28 @@ describe('_', function () {
     it('is a function', function() {
       expect(_.identity).to.be.a('function');
     });
+    it('returns a value that is passed into the function', function() {
+      expect(_.identity('a')).to.be.equal('a');
+    });
+
+    it('returns the same array that is passed into the function', function() {
+      expect(_.identity([1, 2, 3])).to.be.eql([1, 2, 3]);
+    });
+
+    it('returns the same object that is passed into the function', function() {
+      expect(_.identity({a:1, b:2, c:3})).to.be.eql({a:1, b:2, c:3});
+    });
   });
+});
+
+describe.only('#values', function () {
+  it('is a function', function() {
+    expect(_.values).to.be.a('function');
+  });
+  it('returns an empty array when passed a single value', function() {
+    expect(_.values(2)).to.be.eql([]);
+  });
+  it('returns an array with object values when passed an object', function (){
+    expect()
+  })
 });
